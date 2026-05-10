6dbed5faf3a42be9e25ecb1c4c5d83d412dd5400
zhaoyihua <zhaoyihua@yandex.ru>
2026-05-11 00:33:11 +0300

    Update bd in docker

--- application/settings.py
+++ application/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'application.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": os.getenv("DB_ENGINE"),
+        "ENGINE": os.getenv("DB_ENGINE", "django.db.backends.postgresql"),
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
