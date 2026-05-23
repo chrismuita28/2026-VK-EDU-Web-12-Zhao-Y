fdec4df4f78cb41ec457da64a80b4a8779a1976d
zhaoyihua <zhaoyihua@yandex.ru>
2026-05-23 19:04:27 +0300

    Add gunicorn + nginx

--- application/settings.py
+++ application/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "web"]
 
 
 # Application definition
@@ -144,7 +144,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-MEDIA_URL = "/media/"
+MEDIA_URL = "/uploads/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATICFILES_DIRS = [
---
+++ conf/gunicorn.py
@@ -0,0 +1,6 @@
+bind = "0.0.0.0:8000"
+
+workers = 2
+
+accesslog = "logs/gunicorn_access.log"
+errorlog = "logs/gunicorn_error.log"
